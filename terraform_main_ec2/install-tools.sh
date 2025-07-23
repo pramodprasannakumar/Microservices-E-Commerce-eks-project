bf1a4f36ee1c88de7d96efae61c3a30b7333d48b
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-07-23 20:21:13 +0530

    Update install-tools.sh

--- terraform_main_ec2/install-tools.sh
+++ terraform_main_ec2/install-tools.sh
@@ -1,123 +1,121 @@
 #!/bin/bash
+
+# Update system packages
 sudo yum update -y
+git --version
 
-#---------------git install ---------------
+# Install essential tools
+sudo yum install -y git wget unzip curl yum-utils
 
-sudo yum install git -y
+# Install Java (required for Jenkins)
+sudo dnf install -y java-17-amazon-corretto
+java -version
+
+# Install npm
+sudo dnf install nodejs -y
+node -v
+npm -v
 
 
-#-------java dependency for jenkins------------
-
-sudo dnf install java-17-amazon-corretto -y
-
-#------------jenkins install-------------
+# Install Jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum install jenkins -y
+sudo yum install -y jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
+#systemctl status jenkins
 
-
-# ------------------install terraform ------------------
-
-sudo yum install -y yum-utils
+# Install Terraform
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+sudo yum install -y terraform
+terraform -v
 
-#---------------------------------install tomcat------------------
-#sudo wget url https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.83/bin/apache-tomcat-9.0.83.tar.gz
-#sudo tar -xvzf apache-tomcat-9.0.83.tar.gz #untar
-#cd apache-tomcat-9.0.83
-#cd bin
-#chmod +x startup.sh
+# Install Maven
+sudo yum install -y maven
+mvn -v
 
+# Install ansible
+sudo yum install -y ansible
+ansible --version
 
+# Install kubectl
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/
+kubectl version --client
 
-#---------------------------Maven install -------------
-sudo yum install maven -y
+# Install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin/
+eksctl version
 
-#---------------------------kubectl install ---------------
-sudo curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
-sudo chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin 
-# -----------------------------eksctl install--------------------------------
-sudo curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-sudo mv /tmp/eksctl /usr/local/bin
-
-#---------------------------Helm install--------------------
-#https://github.com/helm/helm/releases
-
+# Install Helm
 wget https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz
-
 tar -zxvf helm-v3.6.0-linux-amd64.tar.gz
-
 sudo mv linux-amd64/helm /usr/local/bin/helm
+chmod +x /usr/local/bin/helm
+rm -rf helm-v3.6.0-linux-amd64.tar.gz linux-amd64
+helm version
 
-chmod 777 /usr/local/bin/helm  # give permissions
-
-#------------------Docker install-------------
-#sudo amazon-linux-extras install docker #linux 2022
-sudo yum install docker -y #linux 2023
+# Install Docker
+sudo yum install -y docker
 sudo usermod -aG docker ec2-user
-sudo usermod -aG docker jenkins 
-newgrp docker
-sudo service docker start
+sudo usermod -aG docker jenkins
+sudo systemctl enable docker
+sudo systemctl start docker
 sudo chmod 777 /var/run/docker.sock
+sudo docker --version  
 
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
 
+# Run SonarQube using Docker
+sudo docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+sudo docker ps
 
-
-
-#----------------------Trivy install---------------
+# Install Trivy
 sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.48.3/trivy_0.48.3_Linux-64bit.rpm
+trivy --version
 
-#------------------Docker install-------------
-#sudo amazon-linux-extras install docker #linux 2022
-
-#------------------sonar install by using docker---------------
-docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+# Install vault
+sudo yum install -y vault
 
 
 
-#---------------------------ArgoCD----------------
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-#----------------Grafana Prometheus-------------------
-helm repo add stable https://charts.helm.sh/stable
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-kubectl create namespace Prometheus
-helm install stable prometheus-community/kube-prometheus-stack -n prometheus
-
-echo "Initialization script completed successfully."
-
-#----------------------sonarQube install-----------------------------------
-#sudo yum -y install wget nfs-utils
-#sudo wget -O /etc/yum.repos.d/sonar.repo http://downloads.sourceforge.net/project/sonar-pkg/rpm/sonar.repo
-#sudo yum -y install sonar
-#-----------------------JFROg-----------------------------
-#sudo wget https://releases.jfrog.io/artifactory/artifactory-rpms/artifactory-rpms.repo -O jfrog-artifactory-rpms.repo;
-
-#sudo mv jfrog-artifactory-rpms.repo /etc/yum.repos.d/;
-
-#sudo yum update && sudo yum install jfrog-artifactory-oss -y
-
-#sudo systemctl start artifactory.service
-
-#------------------ Tomcat-----------------------------
-#docker run -d --name tomcat -p 8089:8080 tomcat:lts-community
-
-
-
-#----------------sql-----------------------
-
-sudo yum install mariadb105-server -y
+# Install MariaDB
+sudo yum install -y mariadb105-server
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
+mysql --version 
+#systemctl status mariadb
 
-#----------------AWS CLI Installation-----------------------
 
+# Install PostgreSQL 
+sudo yum install -y postgresql15 postgresql15-server
+sudo /usr/pgsql-15/bin/postgresql-15-setup initdb
+sudo systemctl enable postgresql-15
+sudo systemctl start postgresql-15
+psql --version
+
+# Install AWS CLI v2
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo apt install unzip
 unzip awscliv2.zip
 sudo ./aws/install
+rm -rf awscliv2.zip aws
+
+echo "✅ Initialization script completed successfully."
+
+# Install ArgoCD
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl get pods -n argocd
+
+# Install Prometheus and Grafana using Helm
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+kubectl create namespace prometheus
+helm install prometheus prometheus-community/kube-prometheus-stack -n prometheus
+kubectl get pods -n prometheus
