9953a06c4bc19c98249ae630c3673c97d1fad192
pramodprasanna17@gmail.com <pramodprasanna17@gmail.com>
2025-09-26 16:36:47 +0530

    changed to new ami id

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "aluruarumullaa1"
+  bucket = "pramodvishwakarma1"
 
   tags = {
-    Name        = "aluruarumullaa1"
+    Name        = "pramodvishwakarma1"
     Environment = "dev"
   }
 }
@@ -19,10 +19,10 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "arumullaaluruu1"
+  bucket = "pramodvishwakarma2"
 
   tags = {
-    Name        = "arumullaaluruu1"
+    Name        = "pramodvishwakarma2"
     Environment = "dev"
   }
 }
--- terraform_main_ec2/variables.tf
+++ terraform_main_ec2/variables.tf
@@ -64,19 +64,19 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0150ccaf51ab55a51" // Replace with the latest AMI ID for your region
+  default     = "ami-08982f1c5bf93d976" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.large"
+  default     = "m7i-flex.large"
 }
 
 variable "key_name" {
   description = "EC2 keypair"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-1"       // create a keypair in the AWS console and provide the name here 
 }
 
 variable "instance_name" {
